73a9a8c2f09b021f0dc4894f3118bb4abf64eba2
niko1499 <nxgamarra@gmail.com>
2019-01-19 16:18:01 -0500

    Tester looped

--- Stepper_Test_N.py
+++ Stepper_Test_N.py
@@ -5,10 +5,17 @@ import time
 m1 = PiMotor.Stepper("STEPPER1")
 
 # Rotate Stepper 1 Contiously in forward/backward direction
-m1.forward(0.1,10)  # Delay and rotations
-m1.disable()
+
+delay = .01
+
+for x in range(10):
+    delay = delay + .01
+    print("delay: ", delay)
+    m1.forward(delay, 10)  # Delay and rotations
+    m1.disable()
+    time.sleep(4)
 # for i in range(5):
-    # m1.forward(0.1,10)  # Delay and rotations
-    # time.sleep(2)
-    # m1.backward(0.1,10)
-    # time.sleep(2)
+# m1.forward(0.1,10)  # Delay and rotations
+# time.sleep(2)
+# m1.backward(0.1,10)
+# time.sleep(2)
